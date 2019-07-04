53bc5c95fa5e68746d8b1674c5b39d1424b2114a
Peter Todorov <petodorov@gmail.com>
2019-07-04 11:51:58 +0300

    clearing

--- backend/router/jarRoutes.js
+++
@@ -1,7 +0,0 @@
-const express  = require('express');
-const router = express.Router();
-const {createJar,editJar} = require('../controllers/jarsController')
-
-router.post('/create',createJar)
-router.get('/edit/:id',editJar)
-module.exports = router;
--- backend/router/oliveRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {getOlives} = require('../controllers/olivesController')
-
-router.get('/',getOlives);
-
-module.exports = router;
